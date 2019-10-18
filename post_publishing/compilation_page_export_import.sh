e43e5792bee2cc1fcff07e38aa36599a7a061d0a
Carmen Maria Lanz (Aquila) <elemmire@ossiriand.de>
2019-10-18 11:22:49 +0200

    update shell script for updating BL page numbers in hgv

--- post_publishing/compilation_page_export_import.sh
+++ post_publishing/compilation_page_export_import.sh
@@ -13,17 +13,23 @@ user_hgv=$(sed -n 's/.*user_hgv *= *\([^ ]*.*\)/\1/p' < $ini)
 gloin_hgv=$(sed -n 's/.*password_hgv *= *\([^ ]*.*\)/\1/p' < $ini)
 
 echo "today: $today"
-echo "database: $database"
-echo "user: $user"
+echo "database bl: $database_bl"
+echo "user bl: $user_bl"
+echo "database hgv: $database_hgv"
+echo "user hgv: $user_hgv"
 
 echo "---- export compilation page from bl mysql"
-mysql -u $user_bl -p $gloin_bl < compilation_page_export.sql > compilation_page_import.sql
+#mysql -u $user_bl --password=$gloin_bl < compilation_page_export.sql > compilation_page_import.sql
+# nach diesem Schritt anhalten, um das Ergebnis auf Wohlgefaellingkeit zu ueberpruefen
 
 echo "---- import compilation page into hgv mysql"
-mysql $database_hgv -u $user_hgv -p $gloin_hgv < compilation_page_import.sql
+mysql $database_hgv -u $user_hgv --password=$gloin_hgv < compilation_page_import.sql
+
+echo "---- update blOnline flag in hgv aquila"
+mysql $database_hgv -u $user_hgv --password=$gloin_hgv < hgv_blOnline_update.sql
 
 echo "---- export bl bibliography from hgv mysql"
-mysql $database_hgv -u $user_hgv -p $gloin_hgv < bl_bibliography_export.sql > bl_bibliography_import.csv
+mysql $database_hgv -u $user_hgv --password=$gloin_hgv < bl_bibliography_export.sql > bl_bibliography_import.csv
 
 echo "---- import bl bibliography into hgv FileMaker"
 echo "must be done manually, see README"
