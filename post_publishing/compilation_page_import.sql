8bb3bf58f35eaa48ad4d0ffe5d40e1f3d6abaaeb
Carmen Maria Lanz (Aquila) <elemmire@ossiriand.de>
2019-10-18 11:13:26 +0200

    import bl bibliography into hgv aquila mysql database only when respective volume has not already been imported for a particular data set

--- post_publishing/compilation_page_import.sql
+++ post_publishing/compilation_page_import.sql
@@ -1,3 +1,3 @@
 -- IMPORT_SQL
-UPDATE hgv SET bl = CONCAT(bl, IF(bl IS NULL OR bl = '', '', '; '), 'XIII, S. 2') WHERE hgvId = '28310';
-UPDATE hgv SET bl = CONCAT(bl, IF(bl IS NULL OR bl = '', '', '; '), 'XIII, S. 213') WHERE hgvId = '14599';
+UPDATE hgv SET bl = CONCAT(bl, IF(bl = '' OR bl IS NULL, '', '; '), 'XIII, S. 2') WHERE hgvId = '10031' AND bl NOT LIKE '%XIII, S.%';
+UPDATE hgv SET bl = CONCAT(bl, IF(bl = '' OR bl IS NULL, '', '; '), 'XIII, S. 281/282') WHERE hgvId = '76333' AND bl NOT LIKE '%XIII, S.%';
