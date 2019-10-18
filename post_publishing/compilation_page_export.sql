bfc16626a74ea9a64f5c6e9655f2090de30a259f
Carmen Maria Lanz (Aquila) <elemmire@ossiriand.de>
2019-10-18 11:10:25 +0200

    export compilation page (order, format, etc.)

--- post_publishing/compilation_page_export.sql
+++ post_publishing/compilation_page_export.sql
@@ -3,15 +3,37 @@
 USE bl;
 
 SELECT
-  concat("UPDATE hgv SET bl = CONCAT(bl, IF(bl IS NULL OR bl = '', '', '; '), ", "'", compilation.title, ", S. ", compilationPage,"') WHERE hgvId = '", hgv, "';") AS '-- IMPORT_SQL'
+  concat("UPDATE hgv SET bl = CONCAT(bl, IF(bl = '' OR bl IS NULL, '', '; '), ", "'", compilation, ", S. ", GROUP_CONCAT(DISTINCT page ORDER BY CAST(page AS UNSIGNED) SEPARATOR '/'),"') WHERE hgvId = '", hgv, "' AND bl NOT LIKE '%", compilation, ", S\.%';") AS '-- IMPORT_SQL'
 FROM
-  correction,
-  compilation
+  bl
 WHERE 
-  correction.compilation_id = compilation.id AND 
-  compilation.title = 'XIII' AND
+  compilation = 'XIII' AND
   hgv IS NOT NULL AND
   hgv != '' AND
-  compilationPage IS NOT NULL 
+  description NOT LIKE CONCAT(CONVERT(UNHEX('E28692') USING UTF8), ' Nd.%') AND /* → Nd.% */
+  description NOT LIKE '= %' AND
+  page IS NOT NULL
+GROUP BY
+  hgv
 ORDER BY
-  CAST(compilationPage AS UNSIGNED);
+  CAST(page AS UNSIGNED);
+
+/*
+
+
+*/
+
+/*
+
+-- TEST resulting pages
+
+SELECT `hgv`, ddb, CONCAT('XIII, S. ', GROUP_CONCAT(DISTINCT page ORDER BY CAST(page AS UNSIGNED) SEPARATOR '/'))
+FROM `bl`
+WHERE hgv IS NOT NULL
+AND compilation = 'XIII'
+AND description NOT LIKE '→ Nd\.%'
+AND description NOT LIKE '= %'
+GROUP BY hgv
+ORDER BY CAST(page AS UNSIGNED)
+
+*/
