8919aa5df8692de3b8da99a1b215a7f9a663dda2
root <ul122@uni-heidelberg.de>
2022-04-13 11:55:04 +0200

    auto save 2022.04.13

--- dump/dump.sh
+++ dump/dump.sh
@@ -30,22 +30,22 @@ echo "database: $database" >> $log
 echo "user: $user" >> $log
 
 echo "---- git fetch" >> $log
-git --git-dir "$repo/.git" fetch >> $log 2>&1
+sudo git --git-dir "$repo/.git" fetch >> $log 2>&1
 echo "---- git merge" >> $log
-git --git-dir "$repo/.git" merge edelweiss/master >> $log 2>&1
+sudo git --git-dir "$repo/.git" merge edelweiss/master >> $log 2>&1
 echo "---- mysqldump sql" >> $log
 mysqldump --single-transaction --password=$gloin --user=$user --ignore-table=$database.user $database > $sql
 echo "---- mysqldump xml" >> $log
 mysqldump --single-transaction --password=$gloin --user=$user --ignore-table=$database.user --xml $database > $xml
 echo "---- git add" >> $log
 cd $repo
-git add dump >> $log 2>&1
+sudo git add dump >> $log 2>&1
 echo "---- git commit" >> $log
-git --git-dir "$repo/.git" commit -m "auto save $today" >> $log 2>&1
+sudo git --git-dir "$repo/.git" commit -m "auto save $today" >> $log 2>&1
 echo "---- git gc" >> $log
-git --git-dir "$repo/.git" gc >> $log 2>&1
+sudo git --git-dir "$repo/.git" gc >> $log 2>&1
 echo "---- git push" >> $log
-git --git-dir "$repo/.git" push edelweiss master:master >> $log 2>&1
+sudo git --git-dir "$repo/.git" push edelweiss master:master >> $log 2>&1
 
 date >> $log
 
