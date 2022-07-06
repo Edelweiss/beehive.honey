b36ac0f3f5ca2a72b98df209598f74188029f2db
Elemmire <ul122@uni-heidelberg.de>
2022-07-06 13:19:44 +0200

    auto save 2022-07-06T13:19:33+02:00

--- dump/dump.sh
+++ dump/dump.sh
@@ -1,17 +1,29 @@
 #!/usr/bin/env bash
 #
-#
+# {Authors: Carmen Maria Lanz, Martin Hettich}
 # (Sichern der Daten aus der beehive-Datenbank als XML nach git)
+# (Soll als cronjob laufen, also _nur_im_Fehlerfall_ Ausgaben auf stdout oder stderr produzieren.)
 
-set -e
-set -x
-
-export PATH=$PATH:/usr/local/bin:/usr/local/git/bin
-export CLASSPATH=$CLASSPATH:$HOME/Library/saxon/saxon9he.jar
-
+### Preconditions:
 #### in welchem Verzeichnis muß dieses Skript gestartet werden?   : egal, es wechselt => /home/ubuntu/beehive.honey
 #### von welchem User?   : ubuntu
-#### mit welchen git-credentials?   : 
+#### mit welchen git-credentials?   : ubuntu/.ssh
+
+### Invariants:
+
+### Postconditions:
+
+set -e   ### DEBUGGING: Fail on any error! (AKA: exit immediately on non-zero return)
+# set -x ### DEBUGGING: eXplizitly echo commands before executing (AKA: print command trace)
+
+
+# check and set lock file or exit:
+if [[ -f $(dirname $0)/dump-in-progress.lock ]]; then exit 1; fi
+touch $(dirname $0)/dump-in-progress.lock
+
+
+export PATH=$PATH:/usr/local/bin
+export CLASSPATH=$CLASSPATH:$HOME/Library/saxon/saxon9he.jar
 
 ini="$(dirname $0)/../environment.ini"
 repo=$(sed -n 's/.*repo *= *\([^ ]*.*\)/\1/p' < $ini)
@@ -21,11 +33,11 @@ if [[ -f $log ]]  ### vorhandenes Log umbenennen:
    then mv -f $log ${log}.old  ### noch älteres Log wird überschrieben.
 fi
 
-echo "uid=`whoami`  pwd=`pwd`  ini=$ini  repo=$repo  log=$log"  ### Verbosity
+### start new log file:
+echo -e "---\nStarting log: $(date --iso=s)" > $log
 
-date --iso=s > $log
-
-echo "---- initialize: This is $0 running as $USER on $(hostname -f) in $PWD" >> $log
+echo "---- initialize: This is $0 running as $(whoami) on $(hostname -f) in ${PWD}," >> $log
+echo "---- with variables:  ini=$ini  repo=$repo  log=$log" >> $log
 echo "PATH: $PATH" >> $log
 echo "CLASSPATH: $CLASSPATH" >> $log
 
@@ -36,37 +48,39 @@ database=$(sed -n 's/.*database *= *\([^ ]*.*\)/\1/p' < $ini)
 user=$(sed -n 's/.*user *= *\([^ ]*.*\)/\1/p' < $ini)
 gloin=$(sed -n 's/.*password *= *\([^ ]*.*\)/\1/p' < $ini)
 
-echo "today: $today" >> $log  ### Diese 5 Echos passen auf eine Zeile.
-echo "sql: $sql  xml: $xml  database: $database  user: $user" >> $log
+echo "---- and variables:  today: $today  sql: $sql  xml: $xml  database: $database  user: $user" >> $log
 
-### !!! ### sudo -i -u ubuntu  ### Warum eine Login-shell? Das bleibt hängen bzw. wird interaktiv.
-### wozu sudo -u ubuntu? für root's crontab? => umziehen in ubuntu's crontab!
 
 echo "---- git fetch" >> $log
 git --git-dir "$repo/.git" fetch >> $log 2>&1
+
 echo "---- git merge" >> $log
 git --git-dir "$repo/.git" merge edelweiss/master >> $log 2>&1
+
+
 echo "---- mysqldump sql" >> $log
 mysqldump --single-transaction --password=$gloin --user=$user --ignore-table=$database.user $database > $sql
+
 echo "---- mysqldump xml" >> $log
 mysqldump --single-transaction --password=$gloin --user=$user --ignore-table=$database.user --xml $database > $xml
+
 
 echo "---- git add" >> $log
 cd $repo
 git add dump >> $log 2>&1
+
 echo "---- git commit" >> $log
 git --git-dir "$repo/.git" commit -m "auto save $today" >> $log 2>&1
+
 echo "---- git gc" >> $log
 git --git-dir "$repo/.git" gc >> $log 2>&1
+
 echo "---- git push" >> $log
 git --git-dir "$repo/.git" push edelweiss master:master >> $log 2>&1
 
-date --iso=s >> $log  ### Tipp: Das Kommando 'time <name of command or script>' zeigt auch die benötigte Zeit an.
+echo -e "Finishing log: $(date --iso=s)\n..." >> $log
 
-### cat $log  ### Debugging? Unnötig wenn von cron aus gestartet wird.
-
-### git status  ### Verbosity
-### git log|head  ### Verbosity
-### echo "uid=`whoami`  pwd=`pwd`  ini=$ini  repo=$repo  log=$log"  ### Verbosity
+# remove lock file:
+rm -f $(dirname $0)/dump-in-progress.lock
 
 exit 0
